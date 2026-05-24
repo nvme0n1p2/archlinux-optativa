3800a0d9842305bad99d7e6f88d14a8708c1484c
nyae <z20011024@gmail.com>
2026-05-24 15:20:49 +0200

    added ability to generate docker images, sort of

--- profiledef.sh
+++ profiledef.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 # shellcheck disable=SC2034
 
-iso_name="Archlinux-Optativa"
+iso_name="archlinux-optativa"
 iso_label="ARCH_$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y%m)"
 iso_publisher="Arch Linux <https://archlinux.org>"
-iso_application="Arch Linux optativa"
+iso_application="Arch Linux Optativa"
 iso_version="$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y.%m.%d)"
 install_dir="arch"
-buildmodes=('iso')
+buildmodes=('iso' 'bootstrap')
 bootmodes=('bios.syslinux'
            'uefi.grub')
 pacman_conf="pacman.conf"
@@ -16,4 +16,5 @@ airootfs_image_tool_options=('-zlzma,109' -E 'ztailpacking')
 bootstrap_tarball_compression=(xz -9e)
 file_permissions=(
   ["/etc/shadow"]="0:0:400"
+  ["/etc/gshadow"]="0:0:0400"
 )
